e0304f28e0309fc7052ebbda811d1bd541465cfe
Tim Loyer <tloyer@directemployers.org>
2019-04-09 11:38:57 -0400

    debug

--- get_menu.php
+++ get_menu.php
@@ -1,12 +1,12 @@
 <?php
 
 chdir(__DIR__);
-$hostname = gethostname();
+$hostname = getenv("APP_URL");
 $menu = shell_exec("./gcafe");
 
 $menu = explode($menu, "\n");
 $menu = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $menu);
-
+dump($menu);
 $menu = explode("\n", $menu);
 $title = array_shift($menu);
 $link = array_pop($menu);
